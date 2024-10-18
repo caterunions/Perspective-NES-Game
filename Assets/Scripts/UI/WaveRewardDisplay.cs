5a4a2a2399641f860d9a53b1769c3e29432ac130
Cate Runions <cate.runions@gmail.com>
2024-10-18 11:20:42 -0500

    Add descriptions to wave rewards

--- Assets/Scripts/UI/WaveRewardDisplay.cs
+++ Assets/Scripts/UI/WaveRewardDisplay.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class WaveRewardDisplay : MonoBehaviour
 {
     [SerializeField]
     private WaveRewards _waveRewards;
+
+    [SerializeField]
+    private TextMeshProUGUI _description;
 
     [SerializeField]
     private WaveRewardIcon[] _waveRewardIcons = new WaveRewardIcon[8];
@@ -15,6 +19,8 @@ public class WaveRewardDisplay : MonoBehaviour
         foreach(WaveRewardIcon icon in _waveRewardIcons)
         {
             icon.OnIconClicked += HandleIconClicked;
+            icon.OnIconEntered += HandleIconEntered;
+            icon.OnIconExit += HandleIconExit;
         }
 
         if(_waveRewards.CurrentTrinketRewards.Length > 0)
@@ -32,6 +38,8 @@ public class WaveRewardDisplay : MonoBehaviour
         foreach (WaveRewardIcon icon in _waveRewardIcons)
         {
             icon.OnIconClicked -= HandleIconClicked;
+            icon.OnIconEntered -= HandleIconEntered;
+            icon.OnIconExit -= HandleIconExit;
         }
     }
 
@@ -45,6 +53,23 @@ public class WaveRewardDisplay : MonoBehaviour
         {
             _waveRewards.SetSpellSelection(spell);
         }
+    }
+
+    private void HandleIconEntered(WaveRewardIcon icon, Trinket trinket, Spell spell)
+    {
+        if (trinket != null)
+        {
+            _description.text = trinket.Description;
+        }
+        else if (spell != null)
+        {
+            _description.text = spell.Description;
+        }
+    }
+
+    private void HandleIconExit(WaveRewardIcon icon, Trinket trinket, Spell spell)
+    {
+        _description.text = "";
     }
 
     private void SetupTrinketIcons()
